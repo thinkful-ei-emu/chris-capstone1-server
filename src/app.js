c945d61ea4c4a4e218f4c06811b7b37112b6188f
Chris Martin <chris.bizunit34@gmail.com>
2019-07-18 11:52:49 -0400

    finished creating the template

--- src/app.js
+++ src/app.js
@@ -2,16 +2,33 @@ require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
-const helmet = require('helment');
+const helmet = require('helmet');
 
 const app = express();
 
-const morganOption = (process.env.NODE_ENV === 'production')
+const { NODE_ENV } = require('./config');
+
+const morganOption = (NODE_ENV === 'production')
   ? 'common'
   : 'tiny';
 
 app.use(morgan(morganOption));
 app.use(helmet());
 app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello, boilerplate!');
+});
+
+app.use(function errorHandler(error, req, res, next) {
+  let response;
+  if (NODE_ENV === 'production') {
+    response = { error: { message: 'server error' } };
+  } else {
+    console.error(error);
+    response = { message: error.message, error };
+  }
+  res.status(500).json(response);
+});
 
 module.exports = app;
